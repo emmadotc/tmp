4d6a784c52bc70fd34980d4078a2040527558dae
emma doth <emmadoth@disroot.org>
2023-04-10 09:55:38 +0200

    remove useless leading 0s

--- deez.c
+++ deez.c
@@ -1,1 +1,1 @@
-int main(int c,char**v){c<2?puts("deez what"):(*(long*)v[1]&0x00000000FFFFFFFF)==0x7374756El?puts("lmao"):puts("fuck you");}
+int main(int c,char**v){c<2?puts("deez what"):(*(long*)v[1]&0xFFFFFFFF)==0x7374756El?puts("lmao"):puts("fuck you");}
